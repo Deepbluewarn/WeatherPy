b53e14833a882a5bc1ccb86971d29ed31c378b8e
bluewarn <n5lp97@gmail.com>
2024-05-03 11:52:46 +0000

    중복 제거, 누락된 함수 추가

--- weatherpy/weatherpy.py
+++ weatherpy/weatherpy.py
@@ -5,17 +5,6 @@ from InquirerPy import prompt
 import fullscreen.fullscreen as fs
 import pandas as pd
 
-config = {}
-init = False
-
-try:
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-except FileNotFoundError:
-    init = True
-except json.JSONDecodeError:
-    print("Error: 설정 파일을 불러오는 중 문제가 발생했습니다. 설정 파일을 삭제하고 다시 실행해주세요.")
-
 def select_region():
     sr_config = {}
     cache_file = 'cache.pkl'
@@ -27,7 +16,7 @@ def select_region():
         df = pd.read_pickle(cache_file)
     else:
         # 캐시된 파일이 존재하지 않으면, 원본 파일을 읽고 그 결과를 캐시 파일에 저장합니다.
-        df = pd.read_excel('st.xlsx')
+        df = pd.read_excel('xy.xlsx')
         df.to_pickle(cache_file)
 
     question_1_res = prompt({
@@ -68,26 +57,30 @@ def select_region():
         return select_region()
 
     sr_config['location'] = (df['격자 X'].to_list()[0], df['격자 Y'].to_list()[0])
-
+    sr_config['location_name'] = selected_location
+    
     return sr_config
 
+######## MAIN ########
 
-# 설정 파일이 없는 경우, 사용자에게 설정을 입력받습니다.
+if __name__ == '__main__':
+    config = {}
+    init = False
 
-if init == True:
-    cache_file = 'cache.pkl'
+    try:
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        init = True
+    except json.JSONDecodeError:
+        print("Error: 설정 파일을 불러오는 중 문제가 발생했습니다. 설정 파일을 삭제하고 다시 실행해주세요.")
 
-    df = pd.DataFrame()
+    # 설정 파일이 없는 경우, 사용자에게 설정을 입력받습니다.
 
-    if os.path.exists(cache_file):
-        # 캐시된 파일이 존재하면, 캐시된 파일을 읽습니다.
-        df = pd.read_pickle(cache_file)
-    else:
-        # 캐시된 파일이 존재하지 않으면, 원본 파일을 읽고 그 결과를 캐시 파일에 저장합니다.
-        df = pd.read_excel('st.xlsx')
-        df.to_pickle(cache_file)
+    if init == True:
+        print("WeatherPy 를 처음 실행하셨군요! 기본 설정을 진행합니다.")
 
-    print("WeatherPy 를 처음 실행하셨군요! 기본 설정을 진행합니다.")
-
-    with open('config.json', 'w') as f:
-        json.dump(select_region(), f)
+        with open('config.json', 'w') as f:
+            json.dump(select_region(), f)
+    
+    fs.initScreen()
